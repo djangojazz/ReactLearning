6e1b67a40de19ec0c1d72e8fdeb5cf49c2230ed6
djangojazz <brettmorin2@gmail.com>
2025-04-28 21:23:40 -0700

    Added Banner jsx

---
+++ vite-project/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/GloboLogo.png';
+
+const Banner = () => {
+    return (
+        <header>
+            <div>
+                <img src={logo} alt="Globomantics logo" />
+            </div>
+        </header>
+    );
+}
+
+export default Banner;
