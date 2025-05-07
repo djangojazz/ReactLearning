de0fcb91ac1f9f08f765fe6c2c5b9b903787aba4
djangojazz <brettmorin2@gmail.com>
2025-05-06 18:29:05 -0700

    Removed cache

--- DOTNET API/HousesApi/HousesApi/Bid.cs
+++
@@ -1,9 +0,0 @@
-namespace HousesApi;
-
-public class Bid
-{
-    public int Id { get; set; }
-    public int HouseId { get; set; }
-    public string Bidder { get; set; }
-    public int Amount { get; set; }
-}
